50e2ef05dedc4dfe6c1bf5c45899a752ac58d0c8
Gustavo Costa <cmtehenz@gmail.com>
2021-01-30 23:08:35 -0300

    :bug: troca de url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log(process.env.REACT_APP_API_URL);
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://exata.modery.com.br',
 });
 
 export default api;
